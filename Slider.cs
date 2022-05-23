55075a053f4015a78dc336d7d31fc08b87b3c227
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-05-23 15:44:14 +0400

    Create Slider.cs

---
+++ Slider.cs
@@ -0,0 +1,11 @@
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }    
+        public string Description { get; set; }
+       
+    }
+
+}
